ef0df68a05529f23f3ade6210ee53b1e1aae0995
Administrator <zhaijunfeng2012@gmail.com>
2023-03-21 22:16:23 +0800

    log json

--- src/main.rs
+++ src/main.rs
@@ -178,18 +178,3 @@ async fn main() {
         }
     }
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
